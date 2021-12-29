9ce66c89899c4b137446084340bd7490bf27041e
Hordon13 <hordonandron@gmail.com>
2021-12-29 18:50:56 +0100

    init strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ae217044ee10c0db516a696247f78d2d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb0a72c6-528c-4a6a-8c4b-a2108fb4f316'
+};
